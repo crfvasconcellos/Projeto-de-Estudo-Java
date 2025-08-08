f065dccf84da9fdfccbfb7b2be4833fcd986772b
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-08-08 15:04:15 -0300

    Criei Um loop de avaliação usando For

--- Alura/Projeto Alura/primeiroprojetoalura/src/loop.java
+++ Alura/Projeto Alura/primeiroprojetoalura/src/loop.java
@@ -4,13 +4,16 @@ public class loop {
     public static void main(String[] args) {
         Scanner leitura =  new Scanner(System.in);
 
-        System.out.println("Diga sua avaliação para o filme de 0 à 10: ");
-
-        double avali = leitura.nextDouble();
+        double medavali = 0;
+        double nota =0;
 
         for (int i = 0; i < 3; i++) {
-
+            System.out.println("Diga sua avaliação para o filme de 0 à 10: ");
+            nota = leitura.nextDouble();
+            medavali = nota + medavali ;
         }
+
+        System.out.println("Média de avaliações = " + medavali/3);
 
     }
 }
