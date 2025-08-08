18d20d53872a23a1bf414193b23ed22c252116f3
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-08-07 21:23:57 -0300

    Comecei a escrever o código em Java

---
+++ primeiroprojetoalura/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Esse é o DevFlix \n");
+
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+
+        System.out.println("Ano de lançamento:" + anoDeLancamento);
+        boolean incluidoNoPlano = false;
+
+        double notaDoFilme = 8.1;
+
+    }
+}
