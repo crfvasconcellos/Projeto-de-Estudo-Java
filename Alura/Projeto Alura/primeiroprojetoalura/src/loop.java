e061d839c9b8b187b10ee381e2bbb5d484907ed8
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-08-08 14:44:24 -0300

    criei uma pasta do curso em videio e Teste do Swing

---
+++ Alura/Projeto Alura/primeiroprojetoalura/src/loop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class loop {
+    public static void main(String[] args) {
+        Scanner leitura =  new Scanner(System.in);
+
+        System.out.println("Diga sua avaliação para o filme de 0 à 10: ");
+
+        double avali = leitura.nextDouble();
+
+        for (int i = 0; i < 3; i++) {
+
+        }
+
+    }
+}
