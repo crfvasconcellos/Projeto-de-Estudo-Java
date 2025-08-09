3c113cea3a7420e738611bca0c8c2a3ec7f84481
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-08-09 10:13:12 -0300

    Fiz um mini programa de transferencia bancária

--- Alura/Atividades Alura/exercicios/src/banco.java
+++ Alura/Atividades Alura/exercicios/src/banco.java
@@ -8,8 +8,62 @@ public class banco {
         System.out.println("Qual o seu nome?");
         String nome = leitor.nextLine();
         System.out.println("Qual o seu tipo de Conta?\n (1)-Corrente\n (2)" +
-                "-Poupança" +
-                " ");
+                "-Poupança\n R: " );
+
         int tipodeconta = leitor.nextInt();
+
+        System.out.println("Qual o seu saldo na conta? ");
+        double saldo = leitor.nextDouble();
+
+        while (true){
+            System.out.println("==================\nDados Cadastrados\n==================");
+            System.out.println(String.format("Nome: %s ",nome));
+            if (tipodeconta==1){
+                System.out.println("Tipo de Conta: Corrente");
+            } else if (tipodeconta==2) {
+                System.out.println("Tipo de Conta: Poupança");
+
+            }
+            System.out.println(String.format("Saldo Inicial: %.2f",saldo));
+
+            System.out.println("\nOperações\n 1- Consultar saldos \n 2- " +
+                    "Receber Valor\n 3-Transferir Valor\n 4- Sair\n Digite a " +
+                    "opcção desejada: ");
+            int escolha = leitor.nextInt();
+
+            switch (escolha){
+                case 1:
+                    System.out.println("O saldo da conta é de "+saldo);
+                    break;
+
+                case 2:
+                    System.out.println("Informe o Valor a Receber: ");
+                    double valor = leitor.nextDouble();
+                    saldo= saldo + valor;
+                    System.out.println("Valor Recebido com Sucesso!\n Agora " +
+                            "Seu saldo é de "+saldo);
+                    break;
+
+                case 3:
+                    System.out.println("Qual Valor deseja Transferir:");
+                    double transferencia = leitor.nextDouble();
+                    saldo = saldo -transferencia;
+                    System.out.println("Valor Transferido com Sucesso!\n " + "Agora " + "Seu saldo é de "+saldo);
+                    break;
+                case 4:
+                    break;
+
+                default:
+                    System.out.println("Valor Inválido! Tente Novamente!");
+            }
+
+            if (escolha== 4){
+                break;
+            }
+
+
+
+        }
+        System.out.println("Programa Encerrado!");
     }
 }
