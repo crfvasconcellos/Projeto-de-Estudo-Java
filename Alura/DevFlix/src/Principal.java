8231032f9fa0180b8327fb3066e685049c792f12
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-08-09 15:37:16 -0300

    Comecei a ver java orientado à objetos

---
+++ Alura/DevFlix/src/Principal.java
@@ -0,0 +1,11 @@
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracao= 180;
+
+        System.out.println(meuFilme);
+    }
+}
