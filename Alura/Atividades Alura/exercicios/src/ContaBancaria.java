5cc9128a99bf25469c59857fef333a36ce715642
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-08-12 16:13:09 -0300

    Create ContaBancaria.java

---
+++ Alura/Atividades Alura/exercicios/src/ContaBancaria.java
@@ -0,0 +1,20 @@
+public class ContaBancaria {
+    private int numeroConta;
+    private double saldo;
+    public String titular;
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+        return saldo;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+}
