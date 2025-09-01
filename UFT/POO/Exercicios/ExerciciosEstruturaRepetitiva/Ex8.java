474d781604b3cf53f62490d2b0f3a3442a57c884
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-09-01 20:17:29 -0300

    Create Ex8.java

---
+++ UFT/POO/Exercicios/ExerciciosEstruturaRepetitiva/Ex8.java
@@ -0,0 +1,33 @@
+package ExerciciosEstruturaRepetitiva;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex8 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+        double salario = sc.nextDouble();
+        double imposto=0;
+
+
+        if (salario <= 2000.00) {
+            // Isento
+        } else if (salario <= 3000.00) {
+            imposto = (salario - 2000.00) * 0.08;
+        } else if (salario <= 4500.00) {
+            imposto = 1000.00 * 0.08 + (salario - 3000.00) * 0.18;
+        } else {
+            imposto = 1000.00 * 0.08 + 1500.00 * 0.18 + (salario - 4500.00) * 0.28;
+        }
+
+        if (imposto == 0.0) {
+            System.out.println("Isento");
+        } else {
+            System.out.printf("R$ %.2f%n", imposto);
+        }
+
+    }
+}
