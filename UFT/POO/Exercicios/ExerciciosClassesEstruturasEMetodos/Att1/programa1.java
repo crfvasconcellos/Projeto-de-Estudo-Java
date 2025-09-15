d4c999a1e26be57ca02a50fe5d8310bdc2c8d7a7
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-09-15 18:49:05 -0300

    Fiz exercicio de Retangulo de POO

---
+++ UFT/POO/Exercicios/ExerciciosClassesEstruturasEMetodos/Att1/programa1.java
@@ -0,0 +1,23 @@
+package ExerciciosClassesEstruturasEMetodos.Att1;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class programa1 {
+    public static void main(String[] args) {
+        Rectangle retangulo = new Rectangle();
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Coloque a Largura e a Altura do Retangulo: ");
+        retangulo.width = sc.nextDouble();
+        retangulo.height = sc.nextDouble();
+
+
+        System.out.println("Área = "+ retangulo.area());
+        System.out.println("Perimetro = "+ retangulo.perimeter());
+        System.out.println("Diagonal = "+ retangulo.diagonal());
+
+    }
+}
