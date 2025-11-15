c7d95f116a52957a796041d2d60dfa90807bf3d6
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-11-15 11:32:17 -0300

    criei a pasta para testar exceções em Java

---
+++ Udemy/Tratando Exceções/Teste/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String[] vetor = sc.nextLine().split(" ");
+
+        int position = sc.nextInt();
+        System.out.println(vetor[position]);
+
+
+
+
+
+    }
+}
