b897d8c5c86118f6ac7e4e939e14ba548c6766d6
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2026-01-02 15:08:54 -0300

    fiz exercicio de vetores

---
+++ Udemy/Exercicios/Vetores/negativos/negativo.java
@@ -0,0 +1,41 @@
+package Vetores.negativos;
+
+import java.util.Scanner;
+
+public class negativo {
+    public static void main(String[] args) {
+
+        int[] numeros = new int[10];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quantos numeros deseja-se digitar? ");
+        int qtd = sc.nextInt();
+
+        int [] negativos = new int[10];
+        int qtdn = 0;
+        for (int i = 0; i < qtd; i++) {
+
+            System.out.println("Digite um numero: ");
+            numeros[i] = sc.nextInt();
+
+            if(numeros[i] < 0){
+                negativos[qtdn] = numeros[i];
+                qtdn++;
+            }
+
+
+        }
+
+        System.out.println("Numeros Negativos = ");
+
+        for (int i = 0; i < qtdn; i++ ){
+
+            System.out.printf("\n%d",negativos[i]);
+
+        }
+
+
+
+
+
+    }
+}
