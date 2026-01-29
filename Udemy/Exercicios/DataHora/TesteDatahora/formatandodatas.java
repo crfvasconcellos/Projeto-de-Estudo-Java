ea973c94c8dd299ddb3eab1226b631d0074aa589
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2026-01-29 16:36:59 -0300

    Atualizando aprendizado

--- Udemy/Exercicios/DataHora/TesteDatahora/formatandodatas.java
+++ Udemy/Exercicios/DataHora/TesteDatahora/formatandodatas.java
@@ -1,7 +1,9 @@
 package DataHora.TesteDatahora;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class formatandodatas {
@@ -10,12 +12,17 @@ public class formatandodatas {
 
         LocalDate d1 = LocalDate.now();
         LocalDateTime d2 = LocalDateTime.now();
+        Instant   i1 = Instant.now();
+
 
         DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
         System.out.println("d1 = " + d1);
-        System.out.println("\n d1 formatado1 = " + d1.format(fmt1));
-        System.out.println("\n d1 formatado2 = " + d2.format(fmt2));
+        System.out.println("d1 formatado1 = " + d1.format(fmt1));
+        System.out.println("d2 formatado2 = " + d2.format(fmt2));
+        System.out.println("Instant formatado3 = " + fmt3.format(i1));
+
 
 
     }
