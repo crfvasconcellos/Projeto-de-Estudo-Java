eea584da6b4a808a167bf467ea93243731d0885b
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2026-04-05 15:47:57 -0300

    Aprendi sobre Set

---
+++ Udemy/Exercicios/Set/Demo1/App/Program.java
@@ -0,0 +1,27 @@
+package Set.Demo1.App;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Set<String> set = new LinkedHashSet<>();
+
+         set.add("TV");
+         set.add("Tablet");
+         set.add("Notebook");
+
+         set.removeIf(x -> x.charAt(0) == 'T');
+
+        System.out.println(set.contains("Notebook"));
+
+        for (String p : set){
+            System.out.println(p);
+        }
+
+
+    }
+}
