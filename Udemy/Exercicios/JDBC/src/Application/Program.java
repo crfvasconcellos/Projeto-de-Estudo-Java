616ab79762004d4bcc2724b81104008f46df3721
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2026-07-13 14:17:09 -0300

    Implementação da conexão com o banco de dados

---
+++ Udemy/Exercicios/JDBC/src/Application/Program.java
@@ -0,0 +1,18 @@
+package JDBC.src.Application;
+
+import JDBC.src.db.DB;
+
+import java.sql.Connection;
+
+public class Program {
+    public static void main(String[] args) {
+
+
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
+
+
+
+
+    }
+}
---
+++ Udemy/Exercicios/JDBC/src/db/DbException.java
@@ -0,0 +1,17 @@
+package JDBC.src.db;
+
+import java.io.Serial;
+
+public class DbException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+
+    public DbException(String msg){
+        super(msg);
+    }
+
+
+
+}
