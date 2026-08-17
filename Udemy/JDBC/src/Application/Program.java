c4a1097832d963241d564ec058df6c2021135411
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2026-08-17 18:33:20 -0300

    Fiz o código de recuperação de dados do meu Banco de Dados relacional SQL

--- Udemy/JDBC/src/Application/Program.java
+++ Udemy/JDBC/src/Application/Program.java
@@ -1,16 +1,37 @@
 package Application;
 
 import db.DB;
+import db.DbException;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     static void main() {
 
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
 
+        try{
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("select * from seller");
 
+            while (rs.next()){
+                System.out.println(rs.getInt("Id") + "," + rs.getString("Name"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
 
 
     }
